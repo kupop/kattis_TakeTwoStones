265aca2f50c591e03650100b6882e4a8af60fab9
Leili <Leili@DESKTOP-JMUT38S>
2022-03-18 15:50:10 +0100

    Add project files.

---
+++ kattis_TakeTwoStones/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace kattis_TakeTwoStones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStones = Convert.ToInt32(Console.ReadLine());
+            if (numberOfStones%2 == 0)
+            {
+                Console.WriteLine("Bob");
+            }
+            else
+            {
+                Console.WriteLine("Alice");
+            }
+        }
+    }
+}
